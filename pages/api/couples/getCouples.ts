ffb2d2b424441d6244b5000f9b5508fcc9e43569
JD <58077867+iJDxX@users.noreply.github.com>
2022-07-01 23:14:22 +0200

    Improve Prisma queries

--- pages/api/couples/getCouples.ts
+++ pages/api/couples/getCouples.ts
@@ -5,31 +5,18 @@ import { authOptions, prisma } from "../auth/[...nextauth]";
 export default async function handlerGetCouples(req: NextApiRequest, res: NextApiResponse): Promise<void>{
     const session = await unstable_getServerSession(req, res, authOptions)
     if (session && session.user && session.user.email){
-        const user = await prisma.user.findUnique({
+        const user = await prisma.user.findUniqueOrThrow({
             where: {
                 email: session.user.email
+            },
+            include: {
+                owns: true,
+                joins: true,
             }
         })
 
-        if (user){
-            const couples = await prisma.couple.findMany({
-                where: {
-                    OR: [
-                        {
-                            creatorId: user.id
-                        },
-                        {
-                            joinerId: user.id
-                        }
-                    ]
-                }
-            })
+            const couples = user.owns.concat(user.joins)
             res.status(200).send(JSON.stringify(couples))
-        }else{
-            res.status(400).send("User not exits")
-        }
-
-        
     }else{
         res.status(403).send({
             error: "Unauthorized"
--- pages/api/createNewCouple/[id].ts
+++ pages/api/createNewCouple/[id].ts
@@ -5,59 +5,48 @@ import { authOptions, prisma } from "../auth/[...nextauth]";
 export default async function handlerPostCouples(req: NextApiRequest, res: NextApiResponse){
     const session = await unstable_getServerSession(req, res, authOptions)
     if (session && session.user && session.user.email){
-        const user = await prisma.user.findUnique({
+        const user = await prisma.user.findUniqueOrThrow({
             where: {
                 email: session.user.email
+            },
+            include: {
+                owns: true,
+                joins: true,
+                sendFReq: true,
+                receiveFReq: true,
             }
         })
-        if (user){
-            const couples = await prisma.couple.findMany({
-                where: {
-                    OR: [
-                        {
-                            creatorId: user.id
-                        },
-                        {
-                            joinerId: user.id
-                        }
-                    ]
-                }
-            })
-            if (couples.length > 2){
-                res.status(301).send("You have the maximum amount of couples permited")
-            }else{
-                const { id } = req.query
+        if ((user.joins.length + user.owns.length) > 2){
+            res.status(301).send("You have the maximum amount of couples permited")
+        }else if (user.owns.length + user.joins.length + user.receiveFReq.length + user.sendFReq.length > 2){
+            res.status(400).send("You have between Couples and Couples Request more than 3")
+        }else{
+            const { id } = req.query
     
-                if (!id){
-                    res.status(400).send("Not Id in the Params")
+            if (!id){
+                 res.status(400).send("Not Id in the Params")
+            }else{
+                const userJoiner = await prisma.user.findUniqueOrThrow({
+                    where: {
+                        id: id.toString()
+                    },
+                    include: {
+                         owns: true,
+                        joins: true,
+                    }
+                })
+                if (userJoiner.owns.length + userJoiner.joins.length > 2){
+                    res.status(302).send("The joiner account has already 3 couples")
                 }else{
-                    const couplesJoiner = await prisma.couple.findMany({
-                        where: {
-                            OR: [
-                                {
-                                    creatorId: id.toString()
-                                },
-                                {
-                                    joinerId: id.toString()
-                                }
-                            ]
+                    await prisma.coupleRequest.create({
+                        data: {
+                            senderId: user.id,
+                            receiverId: id.toString(),
                         }
                     })
-                    if (couplesJoiner.length > 2){
-                        res.status(302).send("The joiner account has already 3 couples")
-                    }else{
-                        await prisma.coupleRequest.create({
-                            data: {
-                                senderId: user.id,
-                                receiverId: id.toString(),
-                            }
-                        })
-                        res.status(200).send("Couple created")
-                    }
+                    res.status(200).send("Friend Request sent")
                 }
             }
-        }else{
-            res.status(400).send("User not exits")
         }
     }else{
         res.status(403).send("Not logged in")
