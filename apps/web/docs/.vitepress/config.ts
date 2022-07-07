c10d8a1d38cf163f9b18cbaee24212a96247c332
JD <58077867+iJDxX@users.noreply.github.com>
2022-07-07 16:43:47 +0200

    docs: :memo: Docs site configured

---
+++ apps/web/docs/.vitepress/config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+    title: 'Couple-APP',
+    description: 'Docs site for Couple-APP',
+    lang: 'en-US',
+    lastUpdated: true,
+    titleTemplate: "Docs",
+
+    themeConfig: {
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/JDDD-dev' },
+            { icon: 'twitter', link: 'https://twitter.com/jezudp' },
+        ],
+        footer: {
+            message: 'Made with ♥ by JD',
+            copyright: 'Copyright © 2022-present JD'
+        }
+    }
+})
